670424e0c28c828fd029c58d99e331253e3db0eb
GrandMovie <2406970186@qq.com>
2024-04-29 16:31:59 +0800

    修改天选时期关闭抽奖功能

--- handler/anchorLotStart.go
+++ handler/anchorLotStart.go
@@ -6,10 +6,12 @@ import "github.com/xbclub/BilibiliDanmuRobot-Core/logic"
 func (w *wsHandler) anchorLot() {
 	// 天选启动
 	w.client.RegisterCustomEventHandler("ANCHOR_LOT_START", func(s string) {
+		// 将 LotteryEnable 设置为 false
+		w.svc.Config.LotteryEnable = false
 		if w.svc.Config.InteractWord || w.svc.Config.EntryEffect {
 			w.svc.Config.InteractWord = false
 			w.svc.Config.EntryEffect = false
-			logic.PushToBulletSender("识别到天选，欢迎弹幕已临时关闭")
+			logic.PushToBulletSender("识别到天选，欢迎、抽奖已临时关闭")
 		}
 	})
 	// 天选中奖
@@ -19,6 +21,7 @@ func (w *wsHandler) anchorLot() {
 		}
 
 		w.svc.Config.EntryEffect = w.svc.Autointerract.EntryEffect
-		logic.PushToBulletSender("天选结束，欢迎弹幕已恢复默认")
+		w.svc.Config.LotteryEnable = true
+		logic.PushToBulletSender("天选结束，欢迎、抽奖功能已恢复默认")
 	})
 }
--- logic/danmu/cmd.go
+++ logic/danmu/cmd.go
@@ -1,9 +1,10 @@
 package danmu
 
 import (
+	"strconv"
+
 	"github.com/xbclub/BilibiliDanmuRobot-Core/logic"
 	"github.com/xbclub/BilibiliDanmuRobot-Core/svc"
-	"strconv"
 )
 
 func DoCMDProcess(msg, uid string, svcCtx *svc.ServiceContext) {
@@ -12,14 +13,18 @@ func DoCMDProcess(msg, uid string, svcCtx *svc.ServiceContext) {
 		case "关闭欢迎弹幕":
 			svcCtx.Config.InteractWord = false
 			svcCtx.Config.EntryEffect = false
+			svcCtx.Config.LotteryEnable = false
 			svcCtx.Autointerract.InteractWord = false
 			svcCtx.Autointerract.EntryEffect = false
+			svcCtx.Autointerract.LotteryEnable = false
 			logic.PushToBulletSender("已临时关闭欢迎弹幕")
 		case "开启欢迎弹幕":
 			svcCtx.Config.InteractWord = true
 			svcCtx.Config.EntryEffect = true
+			svcCtx.Config.LotteryEnable = true
 			svcCtx.Autointerract.InteractWord = true
 			svcCtx.Autointerract.EntryEffect = true
+			svcCtx.Autointerract.LotteryEnable = true
 			logic.PushToBulletSender("已临时开启欢迎弹幕")
 		}
 	}
