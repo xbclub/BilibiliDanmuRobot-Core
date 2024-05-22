1035eba5221afd94a93655d5c094a5188ece0c5e
Hades <wzawzpopo@qq.com>
2024-05-22 13:18:04 +0800

    更加稳妥的下播公告

--- handler/goodbye.go
+++ handler/goodbye.go
@@ -5,7 +5,7 @@ import (
 )
 
 // 下播输出
-func (w *wsHandler) sayGoodbye() {
+func (w *wsHandler) sayGoodbyeByWs() {
 	// 下播输出
 	w.client.RegisterCustomEventHandler("PREPARING", func(s string) {
 		if len(w.svc.Config.GoodbyeInfo) > 0 {
